d8da86857c2703d080b5a52d7012da6368dccefa
1007748140 <87281057+1007748140@users.noreply.github.com>
2023-10-06 13:59:37 -0500

    script.js

--- 2do_corte/cadena y arrays/script.js
+++ 2do_corte/cadena y arrays/script.js
@@ -2,7 +2,7 @@ document.getElementById('enviar').addEventListener('click', function() {
     const fruit = document.getElementById('texto').value.toLowerCase();
     const mensaje = document.getElementById('mensaje');
 
-    const fruitsArray = ['manzana', 'banana', 'pera', 'naranja', 'uva', 'mango', 'sandía', 'kiwi', 'cereza', 'melón', 'frambuesa', 'ciruela', 'limón', 'papaya', 'piña', 'durazno', 'granada', 'higo', 'guayaba', 'mora', 'morera', 'mandarina', 'toronja', 'melocotón', 'maracuyá', 'litchi', 'guanábana', 'pomelo', 'coco', 'carambola', 'kiwi', 'lima', 'membrillo', 'nectarina', 'zarzamora', 'avellana', 'cacahuate', 'almendra', 'anacardo', 'castaña', 'nuez', 'avocado', 'coco', 'dátil', 'feijoa', 'fruta de la pasión', 'níspero', 'pistacho', 'manzana verde', 'manzana roja', 'manzana amarilla', 'manzana rosa', 'manzana Fuji', 'manzana Gala', 'manzana Granny Smith'];
+    const frutas = ['manzana', 'banana', 'pera', 'naranja', 'uva', 'mango', 'sandía', 'kiwi', 'cereza', 'melón', 'frambuesa', 'ciruela', 'limón', 'papaya', 'piña', 'durazno', 'granada', 'higo', 'guayaba', 'mora', 'morera', 'mandarina', 'toronja', 'melocotón', 'maracuyá', 'litchi', 'guanábana', 'pomelo', 'coco', 'carambola', 'kiwi', 'lima', 'membrillo', 'nectarina', 'zarzamora', 'avellana', 'cacahuate', 'almendra', 'anacardo', 'castaña', 'nuez', 'avocado', 'coco', 'dátil', 'feijoa', 'fruta de la pasión', 'níspero', 'pistacho', 'manzana verde', 'manzana roja', 'manzana amarilla', 'manzana rosa', 'manzana Fuji', 'manzana Gala', 'manzana Granny Smith'];
 
     const validar = frutas.includes(fruit);
 
